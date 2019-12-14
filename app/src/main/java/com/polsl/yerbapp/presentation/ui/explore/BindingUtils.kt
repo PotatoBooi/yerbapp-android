15f45c1a0c826aa12549e40459892c5f8b4a5e33
justynias <justynakozik1@gmail.com>
2019-12-14 19:30:45 +0100

    Very basic layout with glide usage

--- app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/BindingUtils.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/BindingUtils.kt
@@ -3,26 +3,27 @@ package com.polsl.yerbapp.presentation.ui.explore
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.polsl.yerbapp.R
+import com.bumptech.glide.request.RequestOptions
 import com.polsl.yerbapp.domain.models.ProductModel
 
+
 @BindingAdapter("productName")
-fun TextView.setSleepDurationFormatted(item: ProductModel) {
+fun TextView.setText(item: ProductModel) {
     text = item.name
 }
 
 @BindingAdapter("productImage")
-fun ImageView.setSleepImage(item: ProductModel) {
-    setImageResource(when (item.photoUrl) {
-        else -> R.drawable.ic_launcher_foreground
-        // 0 -> R.drawable.ic_sleep_0
-        // 1 -> R.drawable.ic_sleep_1
-        // 2 -> R.drawable.ic_sleep_2
-        //
-        // 3 -> R.drawable.ic_sleep_3
-        //
-        // 4 -> R.drawable.ic_sleep_4
-        // 5 -> R.drawable.ic_sleep_5
-        // else -> R.drawable.ic_sleep_active
-    })
+fun ImageView.setImage(url: String) {
+    //val url = item.photoUrl
+    //var requestOptions = RequestOptions()
+    //requestOptions = requestOptions.placeholder(R.drawable.boy_32)
+    GlideApp.with(this).load(url).apply(RequestOptions().override(400)).into(this)
+
+    //setImage(url)
+//    GlideApp.with(this.context)
+//        .load(url)
+//        .into(this)
 }
+
+
+
