47e837a07204f31c3d015db486ca5b6273b59a5d
justynias <justynakozik1@gmail.com>
2019-12-20 22:54:53 +0100

    removed unused code, cleanup

--- app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/ExploreViewModel.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/ExploreViewModel.kt
@@ -10,37 +10,18 @@ import com.polsl.yerbapp.data.ProductsRepository
 import com.polsl.yerbapp.domain.models.ProductModel
 import com.polsl.yerbapp.presentation.base.BaseViewModel
 import com.polsl.yerbapp.presentation.ui.explore.adapters.ProductsDataSource
-
 import com.polsl.yerbapp.presentation.ui.explore.adapters.ProductsListener
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+
 
 
 class ExploreViewModel(private val productsRepository: ProductsRepository) : BaseViewModel(), ProductsListener {
     // TODO: Implement the ViewModel
 
-
-
     lateinit var pagedProducts : LiveData<PagedList<ProductModel>>
-       // get() = _pagedProducts
-   // private val _pagedProducts = MutableLiveData<PagedList<ProductModel>>()
-
-
-    val  products: LiveData<List<ProductModel>>
-        get() = _products
-
-    private val _products = MutableLiveData<List<ProductModel>>()
 
     init {
-        //initProducts()
         initPaging()
-
     }
-
-    private fun initProducts() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _products.postValue(productsRepository.getProducts())
-    }}
 
     override fun onItemClick(item: ProductModel) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -52,6 +33,7 @@ class ExploreViewModel(private val productsRepository: ProductsRepository) : Bas
             .setEnablePlaceholders(true)
             .setPageSize(15)
             .build()
+
         pagedProducts = initializedPagedListBuilder(pagedListConfig).build()
     }
 
