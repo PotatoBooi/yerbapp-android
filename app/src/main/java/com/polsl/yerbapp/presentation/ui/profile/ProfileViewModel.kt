40d0c6c4e2901e986ca81fcb2f5b02efd7a6a6de
justynias <justynakozik1@gmail.com>
2019-12-24 00:56:23 +0100

    binding adapter for non editable edit text

--- app/src/main/java/com/polsl/yerbapp/presentation/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/ui/profile/ProfileViewModel.kt
@@ -2,6 +2,8 @@ package com.polsl.yerbapp.presentation.ui.profile
 
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.polsl.yerbapp.presentation.base.BaseViewModel
 import com.polsl.yerbapp.presentation.usecases.GetCurrentUserCase
@@ -11,6 +13,19 @@ import kotlinx.coroutines.launch
 class ProfileViewModel(private val getCurrentUserCase: GetCurrentUserCase) : BaseViewModel() {
     val loginVisible = ObservableBoolean(true)
     val isAuthorized = ObservableField<String>("")
+    val isEditable = ObservableBoolean(false)
+
+    val username: LiveData<String>
+        get() = _username
+    private val _username = MutableLiveData<String>()
+
+    val email: LiveData<String>
+        get() = _email
+    private val _email = MutableLiveData<String>()
+
+    val rank: LiveData<Int>
+        get() = _rank
+    private val _rank = MutableLiveData<Int>()
 
     init {
         checkAuthStatus()
@@ -19,6 +34,17 @@ class ProfileViewModel(private val getCurrentUserCase: GetCurrentUserCase) : Bas
     fun logoutClick() {
         getCurrentUserCase.logoutUser()
         checkAuthStatus()
+    }
+    fun saveClick() {
+      //TODO
+        isEditable.set(false)
+    }
+    fun editClick(){
+        isEditable.set(true)
+    }
+
+    fun discardClick(){
+        isEditable.set(false)
     }
 
     fun checkAuthStatus() = viewModelScope.launch(Dispatchers.Main) {
--- app/src/main/java/com/polsl/yerbapp/presentation/views/BindingAdapters.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/views/BindingAdapters.kt
@@ -1,10 +1,17 @@
 package com.polsl.yerbapp.presentation.views
 
+import android.R.color
+import android.graphics.*
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.request.RequestOptions
+import com.polsl.yerbapp.R
 import com.polsl.yerbapp.presentation.ui.helpers.GlideApp
+
 
 object BindingAdapters {
     @BindingAdapter("visible")
@@ -17,5 +24,29 @@ object BindingAdapters {
     @JvmStatic
     fun setImage(imageView: ImageView, url: String) {
         GlideApp.with(imageView).load(url).apply(RequestOptions().fitCenter()).into(imageView)
+    }
+    @BindingAdapter("editable")
+    @JvmStatic
+    fun editable(view: View, editable: Boolean){
+
+        view.isLongClickable = editable
+        view.isClickable = editable
+        view.isFocusableInTouchMode = editable
+        view.isFocusable = editable
+//        if(editable){
+//
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//                view.background.colorFilter = BlendModeColorFilter(Color.YELLOW, BlendMode.SRC_ATOP)
+//            } else {
+//                view.background.setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP)
+//            }
+//        }
+        if(editable){
+            view.setBackgroundResource(R.drawable.underline)
+        }
+        else{
+            view.setBackgroundColor(Color.TRANSPARENT)
+        }
+
     }
 }
