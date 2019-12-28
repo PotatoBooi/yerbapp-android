8a636476c02ba1062616ea8fe738eff163312f68
justynias <justynakozik1@gmail.com>
2019-12-28 21:37:10 +0100

    layout and viewModel prepared for binding

--- app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/add_product/AddProductViewModel.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/add_product/AddProductViewModel.kt
@@ -3,9 +3,9 @@ package com.polsl.yerbapp.presentation.ui.explore.add_product
 import android.util.Log
 import androidx.databinding.ObservableArrayList
 import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import com.polsl.yerbapp.domain.models.reponse.graphql.ManufacturerModel
 import com.polsl.yerbapp.domain.models.reponse.graphql.TypeModel
 import com.polsl.yerbapp.presentation.base.BaseViewModel
 import com.polsl.yerbapp.presentation.usecases.ProductsCase
@@ -14,21 +14,24 @@ import com.polsl.yerbapp.presentation.usecases.ProductsCase
 class AddProductViewModel(private val productsCase: ProductsCase) : BaseViewModel() {
     val loading = ObservableBoolean(true)
 
-    val types : LiveData<List<TypeModel>>
-        get() = _types
-    private val _types = MutableLiveData<List<TypeModel>>()
+    val name = ObservableField<String>("")
+    val details = ObservableField<String>("")
 
-    var test = ObservableArrayList<TypeModel>()
-    val testIdx = ObservableInt()
+    val types = ObservableArrayList<TypeModel>()
+    val selectedTypeIndex = ObservableInt(0)
+
+    val manufacturers = ObservableArrayList<ManufacturerModel>()
+    val selectedManufacturerIndex = ObservableInt(0)
+
     init{
-        val list = listOf(TypeModel(name = "0"),TypeModel(name = "1"), TypeModel(name = "3") )
-        _types.postValue(list)
+        // TODO load manufacurers and types
 
-        test.addAll(list)
-        testIdx.set(0)
     }
 
     fun onSaveClick() {
-        Log.d("TYPE INDEX", testIdx.get().toString())
+
+        // TODO validation
+        // TODO Post photo or default photo
+        // TODO add product with url photo adress
     }
 }
