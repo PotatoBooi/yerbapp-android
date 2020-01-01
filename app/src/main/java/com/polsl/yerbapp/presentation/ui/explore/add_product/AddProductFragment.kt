df818325a2531fc33ee38c9d8aff4ab0a274093f
justynias <justynakozik1@gmail.com>
2020-01-01 23:05:48 +0100

    cleanup

--- app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/add_product/AddProductFragment.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/ui/explore/add_product/AddProductFragment.kt
@@ -1,13 +1,10 @@
 package com.polsl.yerbapp.presentation.ui.explore.add_product
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import com.esafirm.imagepicker.features.ImagePicker
@@ -19,18 +16,14 @@ import com.polsl.yerbapp.domain.models.reponse.graphql.ManufacturerModel
 import com.polsl.yerbapp.domain.models.reponse.graphql.TypeModel
 import com.polsl.yerbapp.presentation.base.BaseFragment
 import kotlinx.android.synthetic.main.add_product_fragment.*
-import kotlinx.android.synthetic.main.product_item.*
-import kotlinx.android.synthetic.main.product_item.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
-import java.util.concurrent.atomic.AtomicIntegerArray
 
 
 class AddProductFragment : BaseFragment<AddProductViewModel>() {
     override val viewModel: AddProductViewModel? by viewModel { parametersOf(this) }
     private lateinit var binding: AddProductFragmentBinding
     private lateinit var imageView:ImageView
-    private var dpHeight = 0
 
     companion object {
         fun newInstance() = AddProductFragment()
@@ -48,7 +41,6 @@ class AddProductFragment : BaseFragment<AddProductViewModel>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //sManufacturer.setItems(viewModel!!.manufacturers)
         sManufacturer.setAdapter(HintSpinnerAdapter<ManufacturerModel>(context, viewModel!!.manufacturers, context?.resources?.getString(R.string.MANUFACTURER)))
         sType.setAdapter(HintSpinnerAdapter<TypeModel>(context, viewModel!!.types, context?.resources?.getString(R.string.TYPE)))
 
@@ -65,14 +57,10 @@ class AddProductFragment : BaseFragment<AddProductViewModel>() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
             val images = ImagePicker.getImages(data)
             if(!images.isNullOrEmpty()){
-                //imageView.setImageBitmap(BitmapFactory.decodeFile(images[0].path))
                 viewModel?.productImagePath?.set(images[0].path)
-                viewModel?.isImageSet?.set(true)
+                viewModel?.isImageSet?.set(true) // can one observable depend on another automatically?
             }
-        // TODO bind image here to bytearray?
-
     }
 }
