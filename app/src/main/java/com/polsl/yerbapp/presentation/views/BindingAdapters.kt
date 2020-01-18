f4a72dfc8ed2481d57d8a416b0e21f19f7f46241
justynias <justynakozik1@gmail.com>
2020-01-18 21:15:07 +0100

    inverse biding adapter provided for rating!

--- app/src/main/java/com/polsl/yerbapp/presentation/views/BindingAdapters.kt
+++ app/src/main/java/com/polsl/yerbapp/presentation/views/BindingAdapters.kt
@@ -4,13 +4,12 @@ package com.polsl.yerbapp.presentation.views
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.databinding.BindingAdapter
-import androidx.databinding.InverseBindingAdapter
-import androidx.databinding.InverseBindingListener
+import androidx.databinding.*
 import com.bumptech.glide.request.RequestOptions
 import com.polsl.yerbapp.R
 import com.polsl.yerbapp.presentation.ui.helpers.GlideApp
 import com.willy.ratingbar.BaseRatingBar
+import java.nio.file.WatchEvent
 
 
 object BindingAdapters {
@@ -24,19 +23,21 @@ object BindingAdapters {
     @BindingAdapter("productImage")
     @JvmStatic
     fun setProductImage(imageView: ImageView, url: String?) {
-        if(!url.isNullOrEmpty()){
+        if (!url.isNullOrEmpty()) {
             GlideApp.with(imageView).load(url).apply(RequestOptions().fitCenter()).into(imageView)
-        } else{
-            GlideApp.with(imageView).load(R.drawable.product_default).apply(RequestOptions().fitCenter()).into(imageView)
+        } else {
+            GlideApp.with(imageView).load(R.drawable.product_default)
+                .apply(RequestOptions().fitCenter()).into(imageView)
         }
     }
 
     @BindingAdapter("addProductImage")
     @JvmStatic
-    fun setAddProductImage(imageView: ImageView, path: String?){
-        if(path.isNullOrEmpty()) {
-            GlideApp.with(imageView).load(R.drawable.ic_add_photo).apply(RequestOptions().fitCenter()).into(imageView)
-        }else{
+    fun setAddProductImage(imageView: ImageView, path: String?) {
+        if (path.isNullOrEmpty()) {
+            GlideApp.with(imageView).load(R.drawable.ic_add_photo)
+                .apply(RequestOptions().fitCenter()).into(imageView)
+        } else {
             GlideApp.with(imageView).load(path).apply(RequestOptions().fitCenter()).into(imageView)
         }
     }
@@ -47,24 +48,68 @@ object BindingAdapters {
         GlideApp.with(imageView).load(res).apply(RequestOptions().fitCenter()).into(imageView)
     }
 
-    @BindingAdapter("rate")
-    @JvmStatic
-    fun setRate(bar: BaseRatingBar, rate: Float){
-        if(bar.rating != rate){
-            bar.rating = rate
-        }
-    }
-
     @BindingAdapter("textFloat")
     @JvmStatic
-    fun setText(textView: TextView, value: Float){
+    fun setText(textView: TextView, value: Float) {
         textView.text = String.format("%.1f", value)
     }
+
     @BindingAdapter("isIndicator")
     @JvmStatic
-    fun setIsIndicator(ratingBar: BaseRatingBar, value: Boolean){
+    fun setIsIndicator(ratingBar: BaseRatingBar, value: Boolean) {
         ratingBar.setIsIndicator(value)
     }
+
+        @InverseBindingMethods(
+            InverseBindingMethod(
+                type = com.willy.ratingbar.BaseRatingBar::class,
+                attribute = "rate"
+//                method = "getRate"
+            )
+        )
+
+    class RatingBarBinder {
+        companion object {
+            @BindingAdapter("app:rateAttrChanged")
+            @JvmStatic
+            fun setListener(ratingBar: BaseRatingBar, attrChange: InverseBindingListener){
+                ratingBar.setOnRatingChangeListener { ratingBar, rating, fromUser ->
+                    attrChange.onChange()
+                }
+            }
+
+            @InverseBindingAdapter(attribute = "rate")//, event = "ratingAttrChanged")
+            @JvmStatic
+            fun getRate(ratingBar: BaseRatingBar) = ratingBar.rating
+
+            @BindingAdapter("rate")
+            @JvmStatic
+            fun setRate(ratingBar: BaseRatingBar, rate: Float) {
+                if (ratingBar.rating != rate) {
+                    ratingBar.rating = rate
+                }
+            }
+
+        }
+
+    }
+
+}
+//ratingBar.setOnRatingChangeListener(new BaseRatingBar.OnRatingChangeListener() {
+//    @Override
+//    public void onRatingChange(BaseRatingBar ratingBar, int rating, boolean fromUser) {
+//        Log.e(TAG, "onRatingChange: " + rating);
+//    }
+//});
+
+//    @InverseBindingMethods({
+//        @InverseBindingMethod(
+//            type = com.willy.ratingbar.BaseRatingBar,
+//            attribute = "android:text",
+//            method = "getText"
+//        )}
+//    )
+
 
 //    @InverseBindingAdapter(attribute = "rate")
 //    @JvmStatic
@@ -98,4 +143,5 @@ object BindingAdapters {
 //        }
 //    }
 
-}
+
+
